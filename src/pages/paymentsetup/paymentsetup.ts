4aaca6a1e1a76812cbfedfe37b5aa7e07891f6a8
Colin McParland <colin@tinybird.ca>
2017-10-18 21:40:02 -0700

    Added bank account, had to get rid of cardjs which broke my heart

--- src/pages/paymentsetup/paymentsetup.ts
+++ src/pages/paymentsetup/paymentsetup.ts
@@ -1,14 +1,12 @@
-declare var Card: any;
-
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import 'card/dist/card.js';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { DashboardPage } from '../dashboard/dashboard';
 import { Storage } from '@ionic/storage';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Stripe } from '@ionic-native/stripe';
 
+declare const networkinterface;
 
 /**
  * Generated class for the PaymentsetupPage page.
@@ -17,82 +15,73 @@ import { Stripe } from '@ionic-native/stripe';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-paymentsetup',
   templateUrl: 'paymentsetup.html',
 })
 export class PaymentsetupPage {
 
-  private card: any;
-  private ccinfo: FormGroup;
+  private bankinfo: FormGroup;
   private stateprovlist: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public formBuilder: FormBuilder, private stripe: Stripe, public storage: Storage, public http: Http) {
-  	this.ccinfo = this.formBuilder.group({
-  		number: ['', Validators.required],
-  		name: ['', Validators.required],
-  		cvc: ['', Validators.required],
-  		expiry: ['', Validators.required],
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private formBuilder: FormBuilder, private stripe: Stripe, public storage: Storage, public http: Http) {
+
+  	this.bankinfo = this.formBuilder.group({
+  		account: ['', Validators.required],
+  		transit: ['', Validators.required],
+  		institution: ['', Validators.required],
+  		country: ['', Validators.required],
   		fname: ['', Validators.required],
   		lname: ['', Validators.required],
-  		address: ['', Validators.required],
   		city: ['', Validators.required],
   		stateprov: ['', Validators.required],
-  		zip: ['', Validators.required],
+  		address: ['', Validators.required],
   		dob: ['', Validators.required],
-  		country: ['', Validators.required],
-  	})
+  		zip: ['', Validators.required],
+  	});
 
   	this.stateprovlist = ["BC","ON","NL","NS","PE","NB","QC","MB","SK","AB","NT","NU","YT",'AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+
+
   }
 
   ionViewDidLoad() {
     this.viewCtrl.showBackButton(false);
-    this.card = new Card({
-	    form: '.ccform', 
-	    container: '.card-wrapper', 
-	    width: 300,
-	    debug: true 
-	});
   }
 
 
 	getToken() {
 
 		this.stripe.setPublishableKey('pk_live_ED6VDClDIU0wmckUOaveAV2R');
+
 		var currency = '';
-		if(this.ccinfo.value.country == 'CA') {
+		
+		if(this.bankinfo.value.country == 'CA') {
 			currency = 'CAD';
 		}
 		else {
 			currency = 'USD';
 		}
 	
-		let card = {
-			number: this.ccinfo.value.number.replace(" ", ""),
-			expMonth: Number(this.ccinfo.value.expiry.split('/')[0]),
-			expYear: Number(this.ccinfo.value.expiry.split('/')[1]),
-			cvc: this.ccinfo.value.cvc,
-			name: this.ccinfo.value.fname + ' ' + this.ccinfo.value.lname,
-			address_line1: this.ccinfo.value.address,
-			address_city: this.ccinfo.value.city,
-			address_state: this.ccinfo.value.stateprov,
-			address_country: this.ccinfo.value.country,
-			address_zip: this.ccinfo.value.zip,
-			currency: currency
+		let bank = {
+			account_number: this.bankinfo.value.account,
+			routing_number: this.bankinfo.value.transit + '-' + this.bankinfo.value.institution,
+			account_holder_name: this.bankinfo.value.fname + ' ' + this.bankinfo.value.lname,
+			country: this.bankinfo.value.country,
+			currency: currency,
+			account_holder_type: 'individual',
 		};
 
 		/*  TODO:  CC Validation and error messages  */
 	
-		this.stripe.createCardToken(card)
+		this.stripe.createBankAccountToken(bank)
 			.then(token => this.setToken(token))
-			.catch(error => console.log(error));
+			.catch(error => console.log(error['_body']));
 
 	}
 
 
-	setToken(new_cc_token) {
+	setToken(new_bank_token) {
 
 		var access_token = this.storage.get('access_token');
 		var refresh_token = this.storage.get('refresh_token');
@@ -111,28 +100,46 @@ export class PaymentsetupPage {
 			
 			// Set the POST data
 			// TODO:  Hash password
-			let postParams = {
-			  token: new_cc_token,
-			  user_id: val[2],
-			  country: this.ccinfo.value.country,
-			}
-			
-			// Make the request
-			this.http.post("http://tabme.tinybird.ca/api/user/cctoken", JSON.stringify(postParams), options)
-			  .subscribe(data => {
+			let form = this.bankinfo.value;
 
-			    //  Parse the response into an array
-			    var resp = JSON.parse(data['_body']);
-			   	console.log(resp);
+			let year = form.dob.split('-')[0];
+			let month = form.dob.split('-')[1];
+			let day = form.dob.split('-')[2];
+			let ip = '';
+			var that = this;
 
-			    //  If the status code is 200 move to dashboard and start session
-			    if(resp.status == '200') {
-			      	this.navCtrl.push(DashboardPage);
-			    }
-			}, error => {
-			  console.log(error['_body']);
+			networkinterface.getIPAddress(function (this_ip) { 
+				let postParams = {
+				  token: new_bank_token,
+				  user_id: val[2],
+				  country: form.country,
+				  address: form.address,
+				  zip: form.zip,
+				  dob_month: month,
+				  dob_year: year,
+				  dob_day: day,
+				  fname: form.fname,
+				  lname: form.lname,
+				  ip: this_ip
+				}
+				
+				// Make the request
+				that.http.post("http://tabme.tinybird.ca/api/user/banktoken", JSON.stringify(postParams), options)
+				  .subscribe(data => {
+
+				    //  Parse the response into an array
+				    var resp = JSON.parse(data['_body']);
+
+				    console.log(resp);
+
+				    //  If the status code is 200 move to dashboard and start session
+				    if(resp.status == '200') {
+				      	that.navCtrl.push(DashboardPage);
+				    }
+				}, error => {
+				  console.log(error['_body']);
+				});
 			});
-
 		});
 		
 	}
